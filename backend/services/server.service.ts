df2f21541aebe1e46fa90a3f61b41d60896786cb
btwlouis <56355239+btwlouis@users.noreply.github.com>
2024-04-23 19:20:48 +0200

    feat!: increase limit of displayed servers

--- backend/services/server.service.ts
+++ backend/services/server.service.ts
@@ -11,7 +11,7 @@ export async function insertOrUpdate(serverData: IServerData) {
     await Server.create(serverData);
 }
 
-export async function getAll(amount: number = 200) {
+export async function getAll(amount: number = 2000) {
     // order by clients desc
     return await Server.find({}, { "EndPoint": 1, "Data.serverIconUrl": 1, "Data.hostname": 1, "Data.clients": 1, "Data.mapname": 1, "Data.gametype": 1, "Data.sv_maxclients": 1 })
         .sort({ "Data.clients": -1 })
