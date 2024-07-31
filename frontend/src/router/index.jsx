82501a16de4ebf6508a798346af2d421fd028c45
btwlouis <github@onlouis.de>
2024-07-31 17:29:14 +0200

    fix: use hash router

--- frontend/src/router/index.jsx
+++ frontend/src/router/index.jsx
@@ -1,8 +1,8 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import App from "../components/App";
 import Server from "../components/Server";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <App />,
